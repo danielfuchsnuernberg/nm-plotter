d8e948813208a89862404094486f46dd5a40a34a
danielfuchsnuernberg <daniel.fuchs.nuernberg@web.de>
2026-07-10 13:38:13 +0200

    v192

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    Bump CACHE when you change index.html, so devices pull the new
    copy instead of an old cached one.
    ============================================================ */
-const CACHE = 'nmplotter-v191';
+const CACHE = 'nmplotter-v192';
 const TERRAIN_CACHE = 'nmplotter-terrain';
 
 const SHELL = [
